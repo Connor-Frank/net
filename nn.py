b6b0a35f84189749a45bb605a8a4244c03a19e90
Connor Frank <conjfrnk@gmail.com>
2022-02-10 08:50:14 -0500

    4 spaces

--- nn.py
+++ nn.py
@@ -5,11 +5,11 @@ from tqdm import trange
 
 
 def magic_proc(x):
-  return np.tanh(x)
+    return np.tanh(x)
 
 
 def magic_eval(x):
-  return x * (1 - x)
+    return x * (1 - x)
 
 
 N_STEPS = 1e4
@@ -30,23 +30,23 @@ hid0 = 2 * np.random.random((3, 4)) - 1
 hid1 = 2 * np.random.random((4, 1)) - 1
 
 for i in (t := trange(int(N_STEPS))):
-  l0 = x if i % 2 == 0 else x1
+    l0 = x if i % 2 == 0 else x1
 
-  # get layer results one after the other
-  l1 = magic_proc(np.dot(l0, hid0))
-  l2 = magic_proc(np.dot(l1, hid1))
+    # get layer results one after the other
+    l1 = magic_proc(np.dot(l0, hid0))
+    l2 = magic_proc(np.dot(l1, hid1))
 
-  l2_error = y - l2
-  t.set_description("error: %f" % np.mean(np.abs(l2_error)))
+    l2_error = y - l2
+    t.set_description("error: %f" % np.mean(np.abs(l2_error)))
 
-  # get deltas and errors
-  l2_delta = l2_error * magic_eval(l2)
-  l1_error = l2_delta.dot(hid1.T)
-  l1_delta = l1_error * magic_eval(l1)
+    # get deltas and errors
+    l2_delta = l2_error * magic_eval(l2)
+    l1_error = l2_delta.dot(hid1.T)
+    l1_delta = l1_error * magic_eval(l1)
 
-  # update weights
-  hid1 += l1.T.dot(l2_delta)
-  hid0 += l0.T.dot(l1_delta)
+    # update weights
+    hid1 += l1.T.dot(l2_delta)
+    hid0 += l0.T.dot(l1_delta)
 
 print("\noutput after training:")
 l0 = x
